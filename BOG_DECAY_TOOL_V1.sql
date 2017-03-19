52b35d5b07c0aeaa186936004a7d1cb79455ec6b
Bog <jeffbogg@gmail.com>
2017-03-18 22:04:26 -0400

    Update BOG_DECAY_TOOL_V1.sql

--- BOG_DECAY_TOOL_V1.sql
+++ BOG_DECAY_TOOL_V1.sql
@@ -4,39 +4,7 @@ Search the server browser for A15.
 We are ranked #3 on topgameservers, #5 on gametracket, #11 on battlemetrics as of 3/7.
 US PVP. No exploits, 1x raid on a target per day, 1 person stack limit, no racism, unradable bases allowed, .4 claim radius, 0 admin abuse.
 
---BOG DECAY SYSTEM V1.0
---PART 1: BOG_TIME TIMESTAMP ADDITION TO CHARACTER TABLE
-ALTER TABLE characters ADD COLUMN bog_time timestamp DEFAULT NULL /* replace me */;
-UPDATE characters SET bog_time = CURRENT_TIMESTAMP;
-PRAGMA writable_schema = on;
-UPDATE sqlite_master
-SET sql = replace(sql, 'DEFAULT NULL /* replace me */',
-                       'DEFAULT CURRENT_TIMESTAMP')
-WHERE type = 'table'
-  AND name = 'characters';
-PRAGMA writable_schema = off;
-DROP TRIGGER characters_bog_time_modify;
-CREATE TRIGGER characters_bog_time_modify
-AFTER UPDATE
-ON characters
-FOR EACH ROW
-BEGIN
-UPDATE characters SET bog_time = CURRENT_TIMESTAMP
-WHERE isAlive=new.isAlive
-and id=old.id
-OR killerName=new.killerName
-and id=old.id
-OR guild=new.guild
-and id=old.id
-OR level=new.level
-and id=old.id
-OR rank=new.rank
-and id=old.id
-OR lastTimeOnline=new.lastTimeOnline
-and id=old.id;
-END;
-
---PART 2: DELETES DECAYED PLAYERS, GUILDS, ITEMS, BUILDINGS BASED ON THE BOG_TIME COLUMN ADDITION ABOVE. DELETES EVERYTHING PRIOR TO 7 DAYS AGO FROM CURRENT DB TIME. DOES NOT DELETE PLAYERS IN GUILDS THAT AT LEAST A SINGLE MEMBER ACTIVE.
+-- DELETES DECAYED PLAYERS, GUILDS, ITEMS, BUILDINGS BASED ON THE BOG_TIME COLUMN ADDITION. DELETES EVERYTHING PRIOR TO 7 DAYS AGO FROM CURRENT DB TIME. DOES NOT DELETE PLAYERS IN GUILDS THAT AT LEAST A SINGLE MEMBER ACTIVE.
 --lines are alternating, first for players without guilds, then guilds themselves
 --Funcom added a lastTimeOnline column into the database but I haven't tested if it's working to update the modify timestamp at all even though the trigger is set for it,
 --however the trigger also updates the modify timestamp for other reasons: (death, guild rank change, guild change, level change). So tell your players to either change
