e6578c1df284cd471488b79bd0fc5a5221a4e492
Bog <jeffbogg@gmail.com>
2017-03-28 17:39:38 -0400

    Update BOG_DECAY_TOOL_V2.1.sql

--- BOG_DECAY_TOOL_V2.1.sql
+++ BOG_DECAY_TOOL_V2.1.sql
@@ -3,7 +3,7 @@
 --Updated to run faster without as many date translation
 --Changed the update statement so that if you run this often, if the in game timestamp were ever to break, your characters wouldn't get deleted until they fixed it
 --V 2.2 should include thralls per Chewy's help
-update characters set lastTimeOnline = strftime('%s', 'now') where lastTimeOnline is NULL; --In case timestamp ever breaks, this keeps your characters refreshed now that this field is always populated
+update characters set lastTimeOnline = strftime('%s', 'now') where lastTimeOnline is NULL;
 delete from buildable_health where object_id in (select distinct object_id from buildings where owner_id in (select id from characters where lastTimeOnline < strftime('%s', 'now', '-7 days') and owner_id not in (select distinct guild from characters where lastTimeOnline > strftime('%s', 'now', '-7 days') and guild is not null)));
 delete from buildable_health where object_id in (select distinct object_id from buildings where owner_id in (select guildid from guilds where guildid not in (select distinct guild from characters where lastTimeOnline > strftime('%s', 'now', '-7 days') and guild is not null)));
 delete from building_instances where object_id in (select distinct object_id from buildings where owner_id in (select id from characters where lastTimeOnline < strftime('%s', 'now', '-7 days') and owner_id not in (select distinct guild from characters where lastTimeOnline > strftime('%s', 'now', '-7 days') and guild is not null)));
