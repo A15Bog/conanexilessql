a3c31ce343bb4df0ac6f724fa68faf47b9192beb
Bog <jeffbogg@gmail.com>
2017-03-29 08:05:57 -0400

    Add files via upload

---
+++ prepackaged/RecommendedRebootFile.bat
@@ -0,0 +1,10 @@
+::Please read RecommendedRebootScript.sql for what this contains. This is what I use on my server every reboot.
+::Place this file in a directory with the RecommendedRebootScript.sql.
+::Make sure you backup a copy of your good database into a seperate directory just in case before running.
+::Run.
+@echo off
+for %%a in (game*.db) do (
+echo "%%a"
+sqlite3 %%a < RecommendedRebootScript.sql
+)
+pause
