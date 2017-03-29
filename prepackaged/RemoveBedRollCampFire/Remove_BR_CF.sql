3d93df53327c7dba711dfc71238a2d3a1cc86136
Bog <jeffbogg@gmail.com>
2017-03-29 08:26:55 -0400

    Update and rename prepackaged/Remove_BR_CF.sql to prepackaged/RemoveBedRollCampFire/Remove_BR_CF.sql

--- prepackaged/RemoveBedRollCampFire/Remove_BR_CF.sql
+++ prepackaged/RemoveBedRollCampFire/Remove_BR_CF.sql
@@ -5,3 +5,7 @@ delete from actor_position where id in (select distinct object_id from buildings
 delete from item_inventory where template_id in ('12001','10001');
 delete from buildings where object_id in (select distinct object_id from properties where name like '%Bedroll%' or name like '%CampFire%');
 delete from properties where name like '%Bedroll%' or name like '%CampFire%';
+VACUUM;
+REINDEX;
+ANALYZE;
+pragma integrity_check;
