44c11569dea5c3bb5bc7588998896aff907fa7f1
Bog <jeffbogg@gmail.com>
2017-03-29 08:55:22 -0400

    Create AddRecipePoints.bat

---
+++ prepackaged/AddRecipePoints/AddRecipePoints.bat
@@ -0,0 +1,7 @@
+::Adds 200 recipe points to level 50. The amount can be adjusted in the AddRecipePoints.sql file
+@echo off
+for %%a in (game*.db) do (
+echo "%%a"
+sqlite3 %%a < Remove_BR_CF.sql
+)
+pause
