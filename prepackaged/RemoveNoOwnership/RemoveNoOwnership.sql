c61962b1ab976592a12e4db1d20922d51876837c
Bog <jeffbogg@gmail.com>
2017-03-29 08:59:49 -0400

    Update RemoveNoOwnership.sql

--- prepackaged/RemoveNoOwnership/RemoveNoOwnership.sql
+++ prepackaged/RemoveNoOwnership/RemoveNoOwnership.sql
@@ -7,3 +7,7 @@ delete from item_properties where owner_id in (select distinct owner_id from bui
 delete from properties where object_id in (select distinct object_id from properties where name like '%Player%') and object_id not in (select id from characters) and object_id not in (select guildid from guilds);
 delete from item_inventory where owner_id in (select distinct owner_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
 delete from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds);
+VACUUM;
+REINDEX;
+ANALYZE;
+pragma integrity_check;
