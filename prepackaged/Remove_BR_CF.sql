69f952024859926035eaaeb27a06e61a9265b334
Bog <jeffbogg@gmail.com>
2017-03-29 08:25:31 -0400

    Add files via upload

---
+++ prepackaged/Remove_BR_CF.sql
@@ -0,0 +1,7 @@
+--DELETE ALL BEDROLLS AND CAMPFIRES FROM SERVER - VERIFIED IN PRODUCTION ON 3/8
+delete from buildable_health where object_id in (select distinct object_id from buildings where object_id in (select distinct object_id from properties where name like '%Bedroll%' or name like '%CampFire%'));
+delete from building_instances where object_id in (select distinct object_id from buildings where object_id in (select distinct object_id from properties where name like '%Bedroll%' or name like '%CampFire%'));
+delete from actor_position where id in (select distinct object_id from buildings where object_id in (select distinct object_id from properties where name like '%Bedroll%' or name like '%CampFire%'));
+delete from item_inventory where template_id in ('12001','10001');
+delete from buildings where object_id in (select distinct object_id from properties where name like '%Bedroll%' or name like '%CampFire%');
+delete from properties where name like '%Bedroll%' or name like '%CampFire%';
