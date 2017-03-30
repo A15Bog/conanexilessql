fe0403fb3d47a2c715a33704e3a14885cb5e0637
Bog <jeffbogg@gmail.com>
2017-03-29 22:33:08 -0400

    Update EveryReboot.sql

--- prepackaged/EveryReboot/EveryReboot.sql
+++ prepackaged/EveryReboot/EveryReboot.sql
@@ -49,8 +49,8 @@ delete from characters where id in (select id from characters where lastTimeOnli
 --REMOVE NO OWNERSHIP
 delete from buildable_health where object_id in (select distinct object_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
 delete from building_instances where object_id in (select distinct object_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
-delete from properties where object_id in (select distinct object_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
-delete from actor_position where id in (select distinct object_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
+delete from properties where object_id in (select distinct object_id from buildings where owner_id not in (select id from characters) and object_id not in (select guildid from guilds));
+delete from actor_position where id in (select distinct object_id from buildings where owner_id not in (select id from characters) and id not in (select guildid from guilds));
 delete from item_properties where owner_id in (select distinct owner_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
 delete from properties where object_id in (select distinct object_id from properties where name like '%Player%') and object_id not in (select id from characters) and object_id not in (select guildid from guilds);
 delete from item_inventory where owner_id in (select distinct owner_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
