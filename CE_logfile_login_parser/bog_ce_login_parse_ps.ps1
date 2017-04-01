cd43f5f440f6ae498f054aa4c09d0f04e353a500
Bog <jeffbogg@gmail.com>
2017-03-31 22:01:29 -0400

    Update bog_ce_login_parse_ps.ps1

--- CE_logfile_login_parser/bog_ce_login_parse_ps.ps1
+++ CE_logfile_login_parser/bog_ce_login_parse_ps.ps1
@@ -1,4 +1,4 @@
-﻿#Created by A15 Bog to parse logins for finding exploiters
+#Created by A15 Bog to parse logins for finding exploiters
 #https://github.com/A15Bog/conanexilessql
 function Get-ScriptDirectory
 { 
@@ -11,12 +11,15 @@ else
 Split-Path $script:MyInvocation.MyCommand.Path
 }
 }
-$ScriptDirectory = Get-ScriptDirectory
-$Batchfile = "$ScriptDirectory\bog_ce_login_parse.bat"
-Start-Process -Filepath $Batchfile -WorkingDirectory $ScriptDirectory -NoNewWindow -wait
-$inputfile = "$ScriptDirectory\logins.txt"
+$ScriptDirectory = 'C:\ConanExiles\LOG' #Get-ScriptDirectory
+$logdata = Get-ChildItem -Recurse -Include "$ScriptDirectory\*.log" | select-string userID
+$inputfile = $logdata
 $outputfile = "$ScriptDirectory\logins.csv"
-(Get-Content $inputfile) | Foreach-Object {
+#$Batchfile = "$ScriptDirectory\bog_ce_login_parse.bat"
+#del $inputfile  -erroraction 'SilentlyContinue'
+#del $inputfile  -erroraction 'SilentlyContinue'
+#Start-Process -Filepath $Batchfile -WorkingDirectory $ScriptDirectory -NoNewWindow -wait
+$inputfile | Foreach-Object {
     $_ -replace '[/?]', ',' `
        -replace 'Error:', 'Error ' `
        -replace 'Warning:', 'Warning ' `
@@ -24,19 +27,30 @@ $outputfile = "$ScriptDirectory\logins.csv"
        -replace ':', ',' `
        -replace '[\[]', '' `
        -replace ']', ',' 
-} | Set-Content $inputfile 
-$inputfile2 = Get-Content $inputfile	   
-$output = @() 
+} #| Set-Content $inputfile 
+$inputfile2 = $inputfile
+$output = @()
 foreach($line in $inputfile2)
 	{
 	$Date = ($line -split ",")[1]
 	$Name = ($line -split ",") -match "Name="
 	$SteamID = ($line -split ",") -match "dw_user_id="
 	$Name = $Name -replace "Name=",""
-	$SteamID = $SteamID -replace "dw_user_id=",""
-	$output += "'" + $Date + "','" + $Name + "','" + $SteamID + "'"
+	$SteamID = $SteamID -replace "dw_user_id=","steamid="
+	$LoginCount = ""
+	$output += "`"" + $Date + "`",`"" + $Name + "`",`"" + $SteamID + "`"" + ",`"" + $LoginCount + "`""
 	}
 $output | Set-Content $outputfile
-del $inputfile
-$filedata = import-csv $outputfile -Header date , name , steamid  
+#del $inputfile
+$filedata = import-csv $outputfile -Header Date , Name , SteamID, LoginCount
+foreach ($file in $filedata)
+{
+$SteamID = ""
+$SteamID = $file.SteamID
+#$SteamID = "steamid=76561198041098811"
+$LoginCount = $filedata | where {$_.SteamID -match $SteamID} | measure
+#$LoginCount = $LoginCount.Count
+$file.LoginCount = $LoginCount.Count
+}
+$filedata = $filedata | Sort-Object -Property {$_.LoginCount},{$_.Name},{$_.Date}  -descending
 $filedata | export-csv $outputfile -NoTypeInformation
