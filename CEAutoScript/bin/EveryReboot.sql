b2eacc807186019204bd56b0548de9040e72a0d2
Bog <jeffbogg@gmail.com>
2017-09-02 18:23:28 -0400

    Update EveryReboot.sql

--- CEAutoScript/bin/EveryReboot.sql
+++ CEAutoScript/bin/EveryReboot.sql
@@ -46,7 +46,8 @@ delete from character_stats where char_id in (select id from characters where la
 delete from character_stats where char_id in (select id from characters where lastTimeOnline < strftime('%s', 'now', '-7 days') and guild not in (select distinct guild from characters where lastTimeOnline > strftime('%s', 'now', '-7 days') and guild is not null));
 delete from characters where id in (select id from characters where lastTimeOnline < strftime('%s', 'now', '-7 days') and guild is null);
 delete from characters where id in (select id from characters where lastTimeOnline < strftime('%s', 'now', '-7 days') and guild not in (select distinct guild from characters where lastTimeOnline > strftime('%s', 'now', '-7 days') and guild is not null));
---REMOVE NO OWNERSHIP
+/* Commenting out/removing ON 20170902 due to reports of this resetting some player levels and stats after use, and potentially causing crashes the comment out line below was removed on 20170825 for issues with it having an effect on chests and their contents
+--DELETE OBJECTS FROM PLAYERS OR GUILDS WHO LONGER EXIST
 delete from buildable_health where object_id in (select distinct object_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
 delete from building_instances where object_id in (select distinct object_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
 delete from properties where object_id in (select distinct object_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
@@ -55,6 +56,7 @@ delete from item_properties where owner_id in (select distinct owner_id from bui
 --delete from properties where object_id in (select distinct object_id from properties where name like '%Player%') and object_id not in (select id from characters) and object_id not in (select guildid from guilds);
 delete from item_inventory where owner_id in (select distinct owner_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
 delete from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds);
+*/
 --FIX PLAYERS THAT ARE EXILED
 update actor_position set x='59939.539063', y='310979.625', z='-21411.023438' where x = '1.0' or x = '0.0' or z < '-99999.0' or z = '0.0';
 insert into actor_position (class,map,id,x,y,z,sx,sy,sz,rx,ry,rz,rw) select 'BasePlayerChar_C', 'ConanSandbox', id, '-11875.3369140625','123886.0625', '-9016.935546875', '0.949999988079071', '0.949999988079071', '0.949999988079071', '1.87273170603776e-13', '1.7312404764977e-14', '0.092052161693573', '0.995754182338715' from characters where id in (select id from characters where id not in (select id from actor_position));
