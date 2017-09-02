b9f60c0e1c45a65fdc4a70d8b1e3e6e8cbf39bdc
Bog <jeffbogg@gmail.com>
2017-09-02 18:22:00 -0400

    Update EveryReboot.sql

--- prepackaged/EveryReboot/EveryReboot.sql
+++ prepackaged/EveryReboot/EveryReboot.sql
@@ -46,12 +46,17 @@ delete from character_stats where char_id in (select id from characters where la
 delete from character_stats where char_id in (select id from characters where lastTimeOnline < strftime('%s', 'now', '-7 days') and guild not in (select distinct guild from characters where lastTimeOnline > strftime('%s', 'now', '-7 days') and guild is not null));
 delete from characters where id in (select id from characters where lastTimeOnline < strftime('%s', 'now', '-7 days') and guild is null);
 delete from characters where id in (select id from characters where lastTimeOnline < strftime('%s', 'now', '-7 days') and guild not in (select distinct guild from characters where lastTimeOnline > strftime('%s', 'now', '-7 days') and guild is not null));
---REMOVE NO OWNERSHIP
+/* Commenting out/removing ON 20170902 due to reports of this resetting some player levels and stats after use, and potentially causing crashes the comment out line below was removed on 20170825 for issues with it having an effect on chests and their contents
+--DELETE OBJECTS FROM PLAYERS OR GUILDS WHO LONGER EXIST
 delete from buildable_health where object_id in (select distinct object_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
 delete from building_instances where object_id in (select distinct object_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
 delete from properties where object_id in (select distinct object_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
 delete from actor_position where id in (select distinct object_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
 delete from item_properties where owner_id in (select distinct owner_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
+--delete from properties where object_id in (select distinct object_id from properties where name like '%Player%') and object_id not in (select id from characters) and object_id not in (select guildid from guilds);
+delete from item_inventory where owner_id in (select distinct owner_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
+delete from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds);
+*/
 --delete from properties where object_id in (select distinct object_id from properties where name like '%Player%') and object_id not in (select id from characters) and object_id not in (select guildid from guilds);
 delete from item_inventory where owner_id in (select distinct owner_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
 delete from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds);
