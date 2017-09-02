025aadf4d29794811beefc0c33c6adf39c8fdfb7
Bog <jeffbogg@gmail.com>
2017-09-02 18:25:14 -0400

    Update EveryReboot.sql

--- prepackaged/EveryReboot/EveryReboot.sql
+++ prepackaged/EveryReboot/EveryReboot.sql
@@ -57,9 +57,6 @@ delete from item_properties where owner_id in (select distinct owner_id from bui
 delete from item_inventory where owner_id in (select distinct owner_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
 delete from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds);
 */
---delete from properties where object_id in (select distinct object_id from properties where name like '%Player%') and object_id not in (select id from characters) and object_id not in (select guildid from guilds);
-delete from item_inventory where owner_id in (select distinct owner_id from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds));
-delete from buildings where owner_id not in (select id from characters) and owner_id not in (select guildid from guilds);
 --FIX PLAYERS THAT ARE EXILED
 update actor_position set x='59939.539063', y='310979.625', z='-21411.023438' where x = '1.0' or x = '0.0' or z < '-99999.0' or z = '0.0';
 insert into actor_position (class,map,id,x,y,z,sx,sy,sz,rx,ry,rz,rw) select 'BasePlayerChar_C', 'ConanSandbox', id, '-11875.3369140625','123886.0625', '-9016.935546875', '0.949999988079071', '0.949999988079071', '0.949999988079071', '1.87273170603776e-13', '1.7312404764977e-14', '0.092052161693573', '0.995754182338715' from characters where id in (select id from characters where id not in (select id from actor_position));
